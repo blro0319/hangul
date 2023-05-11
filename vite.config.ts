e13310e0f2b156c5fd2ef4dfa7b21b2ce7adbec8
Blro <gyeongtae020319@gmail.com>
2023-05-11 18:28:09 +0900

    build: Use tsc instead of vite-plugin-dts for type build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "@blro/hangul",
-      fileName: (format) => `hangul.${format}.js`,
+      fileName: "hangul",
     },
   },
-  plugins: [dts()],
 });
