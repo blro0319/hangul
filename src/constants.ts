d90b36f511dc3ce533380be4128d60626eb85503
Blro <gyeongtae020319@gmail.com>
2023-05-11 19:32:05 +0900

    feat: Add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,67 @@
-export const HANGUL_CONSONANTS = [
+/**
+ * **한국어**
+ *
+ * 한글 음절의 시작 유니코드.
+ * "가"의 유니코드 값입니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * The starting Unicode of Hangul syllables.
+ * The Unicode value of "가".
+ */
+export const SYLLABLES_START = 0xac00;
+/**
+ * **한국어**
+ *
+ * 한글 음절의 끝 유니코드.
+ * "힣"의 유니코드 값입니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * The ending Unicode of Hangul syllables.
+ * The Unicode value of "힣".
+ */
+export const SYLLABLES_END = 0xd7a3;
+/**
+ * **한국어**
+ *
+ * 한글 음절의 개수.
+ * 초성 19개, 중성 21개, 종성 27개의 조합으로 이루어져 있습니다.
+ * 종성은 없을 수도 있습니다.
+ *
+ * ∴ 19 × 21 × (27 + 1) = 11,172
+ *
+ * ---
+ *
+ * **English**
+ *
+ * The number of Hangul syllables.
+ * It consists of 19 leading consonants, 21 vowels, and 27 trailing consonants.
+ * Trailing consonants can be empty.
+ *
+ * ∴ 19 × 21 × (27 + 1) = 11,172
+ */
+export const SYLLABLES_COUNT = 11172;
+
+/**
+ * **한국어**
+ *
+ * 한글 자음 목록.
+ *
+ * 유니코드 표 순서대로 정렬되어 있습니다.
+ *
+ * ---
+ * **English**
+ *
+ * Hangul consonants.
+ *
+ * Sorted in Unicode order.
+ */
+export const CONSONANTS = [
   "ㄱ",
   "ㄲ",
   "ㄳ",
@@ -30,3 +93,210 @@ export const HANGUL_CONSONANTS = [
   "ㅍ",
   "ㅎ",
 ] as const;
+
+/**
+ * **한국어**
+ *
+ * 한글 초성 목록.
+ *
+ * 유니코드 표 순서대로 정렬되어 있습니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * Hangul leading consonants.
+ *
+ * Sorted in Unicode order.
+ */
+export const LEADING_CONSONANTS = [
+  "ㄱ",
+  "ㄲ",
+  "ㄴ",
+  "ㄷ",
+  "ㄸ",
+  "ㄹ",
+  "ㅁ",
+  "ㅂ",
+  "ㅃ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅉ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+] as const;
+
+/**
+ * **한국어**
+ *
+ * 한글 종성 목록.
+ * 종성은 없을 수도 있습니다.
+ *
+ * 유니코드 표 순서대로 정렬되어 있습니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * Hangul trailing consonants.
+ * Trailing consonants can be empty.
+ *
+ * Sorted in Unicode order.
+ */
+export const TRAILING_CONSONANTS = [
+  "ㄱ",
+  "ㄲ",
+  "ㄳ",
+  "ㄴ",
+  "ㄵ",
+  "ㄶ",
+  "ㄷ",
+  "ㄹ",
+  "ㄺ",
+  "ㄻ",
+  "ㄼ",
+  "ㄽ",
+  "ㄾ",
+  "ㄿ",
+  "ㅀ",
+  "ㅁ",
+  "ㅂ",
+  "ㅄ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+] as const;
+
+/**
+ * **한국어**
+ *
+ * 한글 모음 목록.
+ * 음절의 중성을 구성합니다.
+ *
+ * 유니코드 표 순서대로 정렬되어 있습니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * Hangul vowels.
+ * Compose the medial of a syllable.
+ *
+ * Sorted in Unicode order.
+ */
+export const VOWELS = [
+  "ㅏ",
+  "ㅐ",
+  "ㅑ",
+  "ㅒ",
+  "ㅓ",
+  "ㅔ",
+  "ㅕ",
+  "ㅖ",
+  "ㅗ",
+  "ㅘ",
+  "ㅙ",
+  "ㅚ",
+  "ㅛ",
+  "ㅜ",
+  "ㅝ",
+  "ㅞ",
+  "ㅟ",
+  "ㅠ",
+  "ㅡ",
+  "ㅢ",
+  "ㅣ",
+] as const;
+
+/**
+ * **한국어**
+ *
+ * 한글 자음 분리 목록.
+ * "ㄲ", "ㄸ", "ㅃ", "ㅆ", "ㅉ"을 제외한 자음 겹낱자를 분리합니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * List of disassembled Hangul consonants.
+ * Disassemble composite consonant except "ㄲ", "ㄸ", "ㅃ", "ㅆ", "ㅉ".
+ */
+export const DISASSEMBLED_CONSONANTS_BY_CONSONANT = {
+  ㄱ: "ㄱ",
+  ㄲ: "ㄲ",
+  ㄳ: "ㄱㅅ",
+  ㄴ: "ㄴ",
+  ㄵ: "ㄴㅈ",
+  ㄶ: "ㄴㅎ",
+  ㄷ: "ㄷ",
+  ㄸ: "ㄸ",
+  ㄹ: "ㄹ",
+  ㄺ: "ㄹㄱ",
+  ㄻ: "ㄹㅁ",
+  ㄼ: "ㄹㅂ",
+  ㄽ: "ㄹㅅ",
+  ㄾ: "ㄹㅌ",
+  ㄿ: "ㄹㅍ",
+  ㅀ: "ㄹㅎ",
+  ㅁ: "ㅁ",
+  ㅂ: "ㅂ",
+  ㅃ: "ㅃ",
+  ㅄ: "ㅂㅅ",
+  ㅅ: "ㅅ",
+  ㅆ: "ㅆ",
+  ㅇ: "ㅇ",
+  ㅈ: "ㅈ",
+  ㅉ: "ㅉ",
+  ㅊ: "ㅊ",
+  ㅋ: "ㅋ",
+  ㅌ: "ㅌ",
+  ㅍ: "ㅍ",
+  ㅎ: "ㅎ",
+} as const;
+/**
+ * **한국어**
+ *
+ * 한글 모음 분리 목록.
+ * "ㅐ", "ㅒ", "ㅔ", "ㅖ"를 제외한 모음 겹낱자를 분리합니다.
+ *
+ * ---
+ *
+ * **English**
+ *
+ * List of disassembled Hangul vowels.
+ * Disassemble composite vowel except "ㅐ", "ㅒ", "ㅔ", "ㅖ".
+ */
+export const DISASSEMBLED_VOWELS_BY_VOWEL = {
+  ㅏ: "ㅏ",
+  ㅐ: "ㅐ",
+  ㅑ: "ㅑ",
+  ㅒ: "ㅒ",
+  ㅓ: "ㅓ",
+  ㅔ: "ㅔ",
+  ㅕ: "ㅕ",
+  ㅖ: "ㅖ",
+  ㅗ: "ㅗ",
+  ㅘ: "ㅗㅏ",
+  ㅙ: "ㅗㅐ",
+  ㅚ: "ㅗㅣ",
+  ㅛ: "ㅛ",
+  ㅜ: "ㅜ",
+  ㅝ: "ㅜㅓ",
+  ㅞ: "ㅜㅔ",
+  ㅟ: "ㅜㅣ",
+  ㅠ: "ㅠ",
+  ㅡ: "ㅡ",
+  ㅢ: "ㅡㅣ",
+  ㅣ: "ㅣ",
+} as const;
