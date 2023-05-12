35bcf116b760b068a3025c74883419356a15eb97
Blro <gyeongtae020319@gmail.com>
2023-05-12 12:14:52 +0900

    test: Strictly check character ranges

--- src/isHangulSyllable/index.spec.ts
+++ src/isHangulSyllable/index.spec.ts
@@ -3,17 +3,24 @@ import { isHangulSyllable } from ".";
 describe("isHangulSyllable", () => {
   it('"가" ~ "힣"', () => {
     for (let i = "가".charCodeAt(0); i <= "힣".charCodeAt(0); i++) {
-      expect(isHangulSyllable(String.fromCharCode(i))).toBe(true);
+      const letter = String.fromCharCode(i);
+      expect(isHangulSyllable(letter), letter).toBe(true);
     }
   });
 
   it('"가" 앞의 문자', () => {
-    expect(isHangulSyllable("a")).toBe(false);
-    expect(isHangulSyllable("ㄱ")).toBe(false);
-    expect(isHangulSyllable("ㅏ")).toBe(false);
+    const left = String.fromCharCode("가".charCodeAt(0) - 1);
+    expect(isHangulSyllable(left), "Left").toBe(false);
+
+    expect(isHangulSyllable("a"), "a").toBe(false);
+    expect(isHangulSyllable("ㄱ"), "ㄱ").toBe(false);
+    expect(isHangulSyllable("ㅏ"), "ㅏ").toBe(false);
   });
   it('"힣" 뒤의 문자', () => {
-    expect(isHangulSyllable("ힰ")).toBe(false);
-    expect(isHangulSyllable("力")).toBe(false);
+    const right = String.fromCharCode("힣".charCodeAt(0) + 1);
+    expect(isHangulSyllable(right), "Right").toBe(false);
+
+    expect(isHangulSyllable("ힰ"), "ힰ").toBe(false);
+    expect(isHangulSyllable("力"), "力").toBe(false);
   });
 });
