cf37ae3122c94191d50ca9e0165a255b97fd30bd
Blro <gyeongtae020319@gmail.com>
2023-05-12 23:22:50 +0900

    docs: Get started page & API TOC

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,16 @@
 import { defineConfig } from "vitepress";
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Hangul.js",
   description: "Handles Korean with JavaScript",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
+      { text: "Get Started", link: "/get-started/" },
+      { text: "API", link: "/api/" },
     ],
-
-    sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-    ],
-
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/blro0319/hangul" },
     ],
   },
 });
---
+++ docs/api/index.md
@@ -0,0 +1,31 @@
+# API
+
+[[toc]]
+
+## disassembleHangul
+
+## disassembleHangulConsonant
+
+## disassembleHangulSyllable
+
+## disassembleHangulToGroups
+
+## disassembleHangulVowel
+
+## getHangulLeadingConsonants
+
+## hangulIncludes
+
+## isHangul
+
+## isHangulConsonant
+
+## isHangulLeadingConsonant
+
+## isHangulSyllable
+
+## isHangulTrailingConsonant
+
+## isHangulVowel
+
+## matchLeadingConsonants
